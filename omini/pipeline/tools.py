6c45fc2aba95e86adf1f6f3f34d43faefc6e5e1b
Yuanshi9815 <yuanshi9815@outlook.com>
2025-04-16 18:05:14 +0800

    init

---
+++ omini/pipeline/tools.py
@@ -0,0 +1,14 @@
+import torch
+import numpy as np
+
+
+def seed_everything(seed: int = 42):
+    torch.backends.cudnn.deterministic = True
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+
+def clip_hidden_states(hidden_states: torch.FloatTensor) -> torch.FloatTensor:
+    if hidden_states.dtype == torch.float16:
+        hidden_states = hidden_states.clip(-65504, 65504)
+    return hidden_states
