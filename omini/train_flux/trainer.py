a6481efa720a1318cb8110c28b2f6b8a07c58bae
Yuanshi9815 <yuanshi9815@outlook.com>
2025-05-09 16:19:07 +0800

    move train function to trianer

--- omini/train_flux/trainer.py
+++ omini/train_flux/trainer.py
@@ -5,6 +5,8 @@ import wandb
 import os
 import yaml
 from peft import LoraConfig, get_peft_model_state_dict
+from torch.utils.data import DataLoader
+import time
 
 from typing import List
 
@@ -321,3 +323,60 @@ class TrainingCallback(L.Callback):
                 f"{self.save_path}/{self.run_name}/output", f"lora_{self.total_steps}"
             )
             pl_module.train()
+
+
+def train(dataset, trainable_model, config):
+    # Initialize
+    is_main_process, rank = get_rank() == 0, get_rank()
+    torch.cuda.set_device(rank)
+    # config = get_config()
+
+    training_config = config["train"]
+    run_name = time.strftime("%Y%m%d-%H%M%S")
+
+    # Initialize WanDB
+    wandb_config = training_config.get("wandb", None)
+    if wandb_config is not None and is_main_process:
+        init_wandb(wandb_config, run_name)
+
+    print("Rank:", rank)
+    if is_main_process:
+        print("Config:", config)
+
+    # Initialize dataloader
+    print("Dataset length:", len(dataset))
+    train_loader = DataLoader(
+        dataset,
+        batch_size=training_config["batch_size"],
+        shuffle=True,
+        num_workers=training_config["dataloader_workers"],
+    )
+
+    # Callbacks for testing and saving checkpoints
+    if is_main_process:
+        training_callbacks = [TrainingCallback(run_name, training_config)]
+
+    # Initialize trainer
+    trainer = L.Trainer(
+        accumulate_grad_batches=training_config["accumulate_grad_batches"],
+        callbacks=training_callbacks if is_main_process else [],
+        enable_checkpointing=False,
+        enable_progress_bar=False,
+        logger=False,
+        max_steps=training_config.get("max_steps", -1),
+        max_epochs=training_config.get("max_epochs", -1),
+        gradient_clip_val=training_config.get("gradient_clip_val", 0.5),
+    )
+
+    setattr(trainer, "training_config", training_config)
+    setattr(trainable_model, "training_config", training_config)
+
+    # Save the training config
+    save_path = training_config.get("save_path", "./output")
+    if is_main_process:
+        os.makedirs(f"{save_path}/{run_name}")
+        with open(f"{save_path}/{run_name}/config.yaml", "w") as f:
+            yaml.dump(config, f)
+
+    # Start training
+    trainer.fit(trainable_model, train_loader)
