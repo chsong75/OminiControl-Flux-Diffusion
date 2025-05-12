8bde3e85a33be6d9195bb04b4b28c62444a50728
Yuanshi9815 <yuanshi9815@outlook.com>
2025-05-12 15:50:43 +0800

    multi_condition training support

--- omini/train_flux/train_multi_condition.py
+++ omini/train_flux/train_multi_condition.py
@@ -1,24 +1,20 @@
 import torch
-from torch.utils.data import DataLoader
-import lightning as L
 import os
-import time
 import random
-import yaml
 
 from PIL import Image, ImageDraw
 
 from datasets import load_dataset
 
-from .trainer import BaseModel, get_config, train
+from .trainer import OminiModel, get_config, train
 from ..pipeline.flux_omini import Condition, convert_to_condition, generate
-from .train_single import ImageConditionDataset
+from .train_spatial_alignment import ImageConditionDataset
 
 
 class ImageMultiConditionDataset(ImageConditionDataset):
     def __getitem__(self, idx):
         image = self.base_dataset[idx]["jpg"]
-        image = image.resize((self.target_size, self.target_size)).convert("RGB")
+        image = image.resize(self.target_size).convert("RGB")
         description = self.base_dataset[idx]["json"]["prompt"]
 
         condition_size = self.condition_size
@@ -57,66 +53,65 @@ class ImageMultiConditionDataset(ImageConditionDataset):
         return return_dict
 
 
-class OminiModel(BaseModel):
-    @torch.no_grad()
-    def generate_a_sample(self, save_path, file_name):
-        condition_size = self.training_config["dataset"]["condition_size"]
-        target_size = self.training_config["dataset"]["target_size"]
+@torch.no_grad()
+def test_function(model, save_path, file_name):
+    condition_size = model.training_config["dataset"]["condition_size"]
+    target_size = model.training_config["dataset"]["target_size"]
 
-        position_delta = self.training_config["dataset"].get("position_delta", [0, 0])
-        position_scale = self.training_config["dataset"].get("position_scale", 1.0)
+    position_delta = model.training_config["dataset"].get("position_delta", [0, 0])
+    position_scale = model.training_config["dataset"].get("position_scale", 1.0)
 
-        generator = torch.Generator(device=self.device)
+    condition_type = model.training_config["condition_type"]
+    test_list = []
+
+    condition_list = []
+    for i, c_type in enumerate(condition_type):
+        if c_type in ["canny", "coloring", "deblurring", "depth"]:
+            image = Image.open("assets/vase_hq.jpg")
+            image = image.resize(condition_size)
+            condition_img = convert_to_condition(c_type, image, 5)
+        elif c_type == "fill":
+            condition_img = image.resize(condition_size).convert("RGB")
+            w, h = image.size
+            x1, x2 = sorted([random.randint(0, w), random.randint(0, w)])
+            y1, y2 = sorted([random.randint(0, h), random.randint(0, h)])
+            mask = Image.new("L", image.size, 0)
+            draw = ImageDraw.Draw(mask)
+            draw.rectangle([x1, y1, x2, y2], fill=255)
+            if random.random() > 0.5:
+                mask = Image.eval(mask, lambda a: 255 - a)
+            condition_img = Image.composite(
+                image, Image.new("RGB", image.size, (0, 0, 0)), mask
+            )
+        else:
+            raise NotImplementedError
+        condition = Condition(
+            condition_img,
+            model.adapter_names[i + 2],
+            position_delta,
+            position_scale,
+        )
+        condition_list.append(condition)
+    test_list.append((condition_list, "A beautiful vase on a table."))
+    os.makedirs(save_path, exist_ok=True)
+    for i, (condition, prompt) in enumerate(test_list):
+        generator = torch.Generator(device=model.device)
         generator.manual_seed(42)
-
-        condition_type = self.training_config["condition_type"]
-        test_list = []
-
-        condition_list = []
-        for i, c_type in enumerate(condition_type):
-            if c_type in ["canny", "coloring", "deblurring", "depth"]:
-                image = Image.open("assets/vase_hq.jpg")
-                image = image.resize(condition_size)
-                condition_img = convert_to_condition(c_type, image, 5)
-            elif c_type == "fill":
-                condition_img = image.resize(condition_size).convert("RGB")
-                w, h = image.size
-                x1, x2 = sorted([random.randint(0, w), random.randint(0, w)])
-                y1, y2 = sorted([random.randint(0, h), random.randint(0, h)])
-                mask = Image.new("L", image.size, 0)
-                draw = ImageDraw.Draw(mask)
-                draw.rectangle([x1, y1, x2, y2], fill=255)
-                if random.random() > 0.5:
-                    mask = Image.eval(mask, lambda a: 255 - a)
-                condition_img = Image.composite(
-                    image, Image.new("RGB", image.size, (0, 0, 0)), mask
-                )
-            else:
-                raise NotImplementedError
-            condition = Condition(
-                condition_img,
-                self.adapter_names[i + 2],
-                position_delta,
-                position_scale,
-            )
-            condition_list.append(condition)
-        test_list.append((condition_list, "A beautiful vase on a table."))
-        os.makedirs(save_path, exist_ok=True)
-        for i, (condition, prompt) in enumerate(test_list):
-            res = generate(
-                self.flux_pipe,
-                prompt=prompt,
-                conditions=condition_list,
-                height=target_size[0],
-                width=target_size[1],
-                generator=generator,
-                model_config=self.model_config,
-                kv_cache=self.model_config.get("independent_condition", False),
-            )
-            file_path = os.path.join(
-                save_path, f"{file_name}_{'|'.join(condition_type)}_{i}.jpg"
-            )
-            res.images[0].save(file_path)
+        
+        res = generate(
+            model.flux_pipe,
+            prompt=prompt,
+            conditions=condition_list,
+            height=target_size[0],
+            width=target_size[1],
+            generator=generator,
+            model_config=model.model_config,
+            kv_cache=model.model_config.get("independent_condition", False),
+        )
+        file_path = os.path.join(
+            save_path, f"{file_name}_{'|'.join(condition_type)}_{i}.jpg"
+        )
+        res.images[0].save(file_path)
 
 
 def main():
@@ -124,7 +119,7 @@ def main():
     config = get_config()
     training_config = config["train"]
 
-    # Initialize dataset and dataloader
+    # Initialize dataset
     dataset = load_dataset(
         "webdataset",
         data_files={"train": training_config["dataset"]["urls"]},
@@ -154,9 +149,10 @@ def main():
         model_config=config.get("model", {}),
         gradient_checkpointing=training_config.get("gradient_checkpointing", False),
         adapter_names=[None, None, *["default"] * cond_n],
+        # In this setting, all the conditions are using the same LoRA adapter
     )
 
-    train(dataset, trainable_model, config)
+    train(dataset, trainable_model, config, test_function)
 
 
 if __name__ == "__main__":
